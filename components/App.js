730c996339b0c1730756f9cde60e77006ae7ac9e
Generosa Litton <glcodeworks@gmail.com>
2019-05-19 14:22:02 -0700

    added routes

--- components/App.js
+++ components/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import Home from './Home'
 import About from './About'
 import Contact from './Contact'
 
 export default class App extends Component {
   render() {
-    return <div id='app-container'>
-      <Home />
-    </div>
+    return (
+      <div id='app-container'>
+        <BrowserRouter>
+          <Route path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+        </BrowserRouter>
+
+      </div>
+    )
   }
 }
